35b4915694dd1612c7f4e3ca9469c0112b2497e0
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-10-30 21:38:22 +0100

    feat: migrate to vite-ts

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="logo-container">
+          <img src="src/assets/white.svg" alt="Logo" />
+        </div>
+      </nav>
+
+      <h1>Welcome to PianoRoll frontend coding challenge!</h1>
+
+      <div id="buttonContainer">
+        <button id="loadCSV">Load Piano Rolls!</button>
+      </div>
+
+      <div id="pianoRollContainer"></div>
+    </>
+  );
+}
+
+export default App;
