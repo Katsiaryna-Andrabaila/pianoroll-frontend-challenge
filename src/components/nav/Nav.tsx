463fe2f76bfdc46061e0d01de3d155e3ec5a04d5
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-11-06 18:46:01 +0100

    fix: fix sources

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -4,7 +4,7 @@ export const Nav = () => {
   return (
     <nav className="navbar">
       <div className="logo-container">
-        <img src="../../../src/assets/white.svg" alt="Logo" />
+        <img src="/white.svg" alt="Logo" />
       </div>
     </nav>
   );
